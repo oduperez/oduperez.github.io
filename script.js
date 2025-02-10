8a65631cfcd1fb0496be21571ad707c681bfaa90
oduperez <146657966+oduperez@users.noreply.github.com>
2025-02-10 23:05:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function moverBoton() {
         var botonNo = document.getElementById('no');
-        var contenedor = document.querySelector('.container');
-        var maxX = contenedor.clientWidth - botonNo.offsetWidth;
-        var maxY = contenedor.clientHeight - botonNo.offsetHeight;
+        var maxX = window.innerWidth - botonNo.offsetWidth;
+        var maxY = window.innerHeight - botonNo.offsetHeight;
         var x = Math.floor(Math.random() * maxX);
         var y = Math.floor(Math.random() * maxY);
-        x = Math.max(0, Math.min(x, maxX - botonNo.offsetWidth));
-        y = Math.max(0, Math.min(y, maxY - botonNo.offsetHeight));
+        x = Math.max(0, Math.min(x, maxX));
+        y = Math.max(0, Math.min(y, maxY));
         botonNo.style.left = x + 'px';
         botonNo.style.top = y + 'px';
     }
