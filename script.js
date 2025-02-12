82cbd6db471304cb9a0fb0b82aa2d960d840f633
oduperez <146657966+oduperez@users.noreply.github.com>
2025-02-12 19:44:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ continueBtn.addEventListener("click", function() {
     // 2. Luego ocultar el texto y el botón
     stageText.classList.add("fade-out");
     continueBtn.classList.add("fade-out");
-  }, 800); // Un pequeño retraso para que primero desaparezcan las imágenes
+  }, 400); // Un pequeño retraso para que primero desaparezcan las imágenes
   
   setTimeout(() => {
     stageIndex++;
@@ -60,13 +60,11 @@ continueBtn.addEventListener("click", function() {
       // 4. Cambiar el texto en el DOM
       stageText.textContent = stages[stageIndex];
 
-      setTimeout(() => {
-        // 5. Mostrar el nuevo texto y botón
-        stageText.classList.remove("fade-out");
-        continueBtn.classList.remove("fade-out");
-        stageText.classList.add("fade-in");
-        continueBtn.classList.add("fade-in");
-      }, 600);
+      // 5. Mostrar el nuevo texto y botón
+      stageText.classList.remove("fade-out");
+      continueBtn.classList.remove("fade-out");
+      stageText.classList.add("fade-in");
+      continueBtn.classList.add("fade-in");
 
       // 6. Esperar antes de hacer aparecer las imágenes nuevas
       setTimeout(() => {
